383a367417cfa4081864764c86da77bf0dea6df0
venzel <eneas.eng@yahoo.com>
2022-01-02 19:15:10 -0300

    feat: cria lista de produtos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
+    constructor(private prisma: PrismaService) {}
+
     create(createProductDto: CreateProductDto) {
         return 'This action adds a new product';
     }
 
     findAll() {
-        return `This action returns all products`;
+        return this.prisma.product.findMany({ where: { published: true } });
     }
 
     findOne(id: number) {
