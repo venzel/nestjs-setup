dbdfbe2e7487dc41d584ae42b623e6a02f68c10f
venzel <eneas.eng@yahoo.com>
2022-01-02 18:50:05 -0300

    feat: cria migration de produto

--- SETUP.md
+++ SETUP.md
@@ -1,3 +1,7 @@
 ```bash
 npx prisma init --datasource-provider postgresql
 ```
+
+```bash
+npx prisma migrate dev --name product
+```
---
+++ prisma/migrations/20220102214939_product/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(65,30) NOT NULL,
+    "sku" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_sku_key" ON "Product"("sku");
